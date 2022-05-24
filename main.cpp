795cd1be00824d726fa1dcc2e60dff5ef25ef526
James <jjcastle@utexas.edu>
2022-05-23 20:36:03 -0500

    Base plan for building out set solution created.

--- main.cpp
+++ main.cpp
@@ -6,8 +6,34 @@
 #include <algorithm>
 using namespace std;
 
-
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    set<int> mySet;
+
+    int numQueries = 0;
+
+    //Data validate numQueries  0 < Q < 1e5
+
+    //Create a count-controlled loop that cycles for n number of queries
+    for (int i = 0; i < numQueries; i++) {
+
+        //get Input (getline where a = switch options x = value)
+        int switchOption = 0;
+        int value = 0;
+
+        //validate 1 < a < 3 && 1 < x < 1e9
+
+        //Swtich
+
+            // Option 1, Add element x to set
+
+            // Option 2, Delete Element x from set if exists, or do nothing if x not in set
+
+            //Option 3, Print yes if x is found in set or no if x is not found in set
+
+            //Default, Do nothing
+
+    }
+
     return 0;
 }
